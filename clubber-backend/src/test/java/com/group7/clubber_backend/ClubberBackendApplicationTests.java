72b5a67531d189fd3ecdcfdff151d98773dc5c6c
ThrawnK <kevinkwzheng@outlook.com>
2025-04-05 13:24:43 -0700

    init

---
+++ clubber-backend/src/test/java/com/group7/clubber_backend/ClubberBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.clubber_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubberBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
